af7b3f6545facaed7ccd555c23305b8b87b4c922
Lucas_hgs <lucas.henrique.gois.santos@gmail.com>
2026-05-19 13:24:40 -0300

    Day 25/26: Configuring zed to work with the venv

--- day_25/main.py
+++ day_25/main.py
@@ -1,20 +1,23 @@
-import csv
-
-# from learning_python_env/lib64/python3.12/site-packages/pandas import pandas
 import pandas
 
 
 def main():
     if __name__ == "__main__":
-        df = pandas.read_csv("weather_data.csv")
-        print(df.to_string())
-        with open("weather_data.csv", mode="r") as data_file:
-            data = csv.reader(data_file)
-            temperatures = []
-            for row in data:
-                if row[1] != "temp":
-                    temperatures.append(int(row[1]))
-        print(temperatures)
+        data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+        gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+        cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+        black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+        data_dict = {
+            "Fur color": ["Gray", "Cinnamon", "Black"],
+            "Count": [
+                gray_squirrels_count,
+                cinnamon_squirrels_count,
+                black_squirrels_count,
+            ],
+        }
+        new_csv = pandas.DataFrame(data_dict)
+        new_csv.to_csv("squirrel_count.csv")
 
 
 main()
